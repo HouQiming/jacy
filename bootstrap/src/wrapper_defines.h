f96e2f19047f9a51fbf5c9b326aeb41549de2a00
HQM <hqm03ster@gmail.com>
2015-01-03 09:55:07 +0800

    new-devenv-up

---
+++ bootstrap/src/wrapper_defines.h
@@ -0,0 +1,32 @@
+#ifndef __WRAPPER_DEFINES_H
+#define __WRAPPER_DEFINES_H
+#ifdef LINUX
+#undef _WIN32
+#endif
+
+#ifndef _WIN32
+	#define __declspec(x)
+	#define __stdcall
+	#define __cdecl
+#endif
+
+#ifdef __cplusplus
+	#if defined(PM_RELEASE)||!defined(_WIN32)
+		#define EXPORT extern "C"
+	#else
+		#define EXPORT extern "C" __declspec(dllexport)
+	#endif
+#else
+	#if defined(PM_RELEASE)||!defined(_WIN32)
+		#define EXPORT
+	#else
+		#define EXPORT __declspec(dllexport)
+	#endif
+#endif
+
+#ifdef _M_AMD64
+typedef long long iptr;
+#else
+typedef long iptr;
+#endif
+#endif
