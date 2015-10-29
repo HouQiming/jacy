5f16bed00c24535b0ec2e0978c11cde2ef8d17c6
HQM <hqm03ster@gmail.com>
2015-10-29 22:59:43 +0800

    backup

---
+++ wrapper/filedlg_mac.m
@@ -0,0 +1,28 @@
+#import <AppKit/AppKit.h>
+#include "wrapper_defines.h"
+
+EXPORT int osal_DoFileDialogMac(char* buf,int szbuf,int is_save){
+	if(is_save){
+		NSSavePanel* saveDlg = [NSSavePanel savePanel];
+		//[panel setAllowedFileTypes:[NSArray arrayWithObject:@"pdf"]];
+		if ( [saveDlg runModal] == NSOKButton ){
+			if([[[saveDlg URL] path] getCString:buf maxLength:szbuf encoding:NSUTF8StringEncoding]) {
+				return 1;
+			}
+		}
+	}else{
+		NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+		[openDlg setCanChooseFiles:YES];
+		[openDlg setCanChooseDirectories:NO];
+		[openDlg setCanChooseDirectories:NO];
+		//[panel setAllowedFileTypes:[NSArray arrayWithObject:@"pdf"]];
+		if ( [openDlg runModal] == NSOKButton ){
+			for( NSURL* URL in [openDlg URLs] ){
+				if([[URL path] getCString:buf maxLength:szbuf encoding:NSUTF8StringEncoding]) {
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
